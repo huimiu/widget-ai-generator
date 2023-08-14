8e9ca489c79f96d0ed137ea62cf3c7b4d7dded94
huimiu <huimiao@microsoft.com>
2023-08-14 22:20:57 +0800

    style: dashboard layout

--- src/dashboards/MyDashboard.tsx
+++ src/dashboards/MyDashboard.tsx
@@ -19,13 +19,16 @@ export default class MyDashboard extends BaseDashboard<any, MyDashboardState> {
   override layout(): JSX.Element | undefined {
     return (
       <>
-        <div>
-          <Input placeholder="Search" />
-          <Button onClick={() => this.askAI()}>ASK</Button>
+        <div className="ask-style">
+          <div>
+            <Input placeholder="Search" />
+            <Button onClick={() => this.askAI()}>ASK</Button>
+          </div>
         </div>
-        {this.state.codeContents && this.state.codeContents.map((codeBlock) => {
-          return <CodeCard content={codeBlock} />;
-        })}
+        {this.state.codeContents &&
+          this.state.codeContents.map((codeBlock) => {
+            return <CodeCard content={codeBlock} />;
+          })}
       </>
     );
   }
