808be8bbe114faccb366161098157cacced6f36e
huimiu <huimiao@microsoft.com>
2023-08-14 23:32:13 +0800

    feat: loading

--- src/dashboards/MyDashboard.tsx
+++ src/dashboards/MyDashboard.tsx
@@ -5,6 +5,13 @@ import { BaseDashboard } from "@microsoft/teamsfx-react";
 
 import { generate } from "../services/generate";
 import { CodeCard } from "../widgets/CodeCard";
+import { Send20Regular } from "@fluentui/react-icons";
+import {
+  CopilotProvider,
+  LatencyWrapper,
+  LatencyLoader,
+  LatencyCancel,
+} from "@fluentai/react-copilot";
 
 interface CodeBlock {
   name: string;
@@ -13,18 +20,40 @@ interface CodeBlock {
 
 interface MyDashboardState {
   codeContents: CodeBlock[];
+  onloading: boolean;
 }
 
 export default class MyDashboard extends BaseDashboard<any, MyDashboardState> {
   override layout(): JSX.Element | undefined {
     return (
       <>
-        <div className="ask-style">
-          <div>
-            <Input placeholder="Search" />
-            <Button onClick={() => this.askAI()}>ASK</Button>
+        {this.state.onloading ? (
+          <div className="generating-style">
+            <CopilotProvider>
+              <LatencyWrapper className="wrapper">
+                <LatencyLoader header={"Generating widget files..."} />
+                <LatencyCancel>Stop generating</LatencyCancel>
+              </LatencyWrapper>
+            </CopilotProvider>
           </div>
-        </div>
+        ) : (
+          <div className="ask-style">
+            <div>
+              <Input
+                root="ask-input"
+                size="large"
+                placeholder="Describe the requirements for generating widget"
+              />
+              <Button
+                onClick={() => this.askAI()}
+                size="large"
+                icon={<Send20Regular />}
+                title="Generate"
+              />
+            </div>
+          </div>
+        )}
+
         {this.state.codeContents &&
           this.state.codeContents.map((codeBlock) => {
             return <CodeCard content={codeBlock} />;
@@ -38,9 +67,11 @@ export default class MyDashboard extends BaseDashboard<any, MyDashboardState> {
   }
 
   private async askAI() {
+    this.setState({ onloading: true, codeContents: [] });
     const resp = await generate("Implement a widget only display a string of Hello world");
     this.setState({
       codeContents: resp,
+      onloading: false,
     });
   }
 }
