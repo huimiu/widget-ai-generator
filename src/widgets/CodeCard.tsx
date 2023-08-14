198ded446aa05c30657cd428916ca184e702bc5e
huimiu <huimiao@microsoft.com>
2023-08-15 00:16:05 +0800

    feat: copy code

--- src/widgets/CodeCard.tsx
+++ src/widgets/CodeCard.tsx
@@ -1,11 +1,18 @@
 import "highlight.js/styles/github.css";
+import "../styles/CodeCard.css";
 
 import hljs from "highlight.js";
 
-import { Button, Text } from "@fluentui/react-components";
+import {
+  Button,
+  Popover,
+  PopoverSurface,
+  PopoverTrigger,
+  Text,
+  ToggleButton,
+} from "@fluentui/react-components";
 import { ArrowDownload24Regular, Code24Regular, Copy24Regular } from "@fluentui/react-icons";
 import { BaseWidget, IWidgetClassNames } from "@microsoft/teamsfx-react";
-import "../styles/CodeCard.css";
 
 export class CodeCard extends BaseWidget<any, any> {
   override header() {
@@ -14,11 +21,18 @@ export class CodeCard extends BaseWidget<any, any> {
         <Code24Regular />
         <Text>{this.props.content.name}</Text>
         <div>
-          <Button
-            icon={<Copy24Regular />}
-            appearance="transparent"
-            onClick={() => this.props.copy(this.props.content.code)}
-          />
+          <Popover size="small" closeOnScroll={true}>
+            <PopoverTrigger>
+              <ToggleButton
+                icon={<Copy24Regular />}
+                appearance="transparent"
+                onClick={() => this.fallbackCopyTextToClipboard(this.props.content.code)}
+              />
+            </PopoverTrigger>
+            <PopoverSurface>
+              <div>Copied</div>
+            </PopoverSurface>
+          </Popover>
           <Button icon={<ArrowDownload24Regular />} appearance="transparent" />
         </div>
       </div>
@@ -50,4 +64,23 @@ export class CodeCard extends BaseWidget<any, any> {
       hljs.highlightBlock(block as HTMLElement);
     });
   };
+
+  fallbackCopyTextToClipboard(text: string) {
+    var textArea = document.createElement("textarea");
+    textArea.value = text;
+
+    textArea.style.top = "0";
+    textArea.style.left = "0";
+    textArea.style.position = "fixed";
+
+    document.body.appendChild(textArea);
+    textArea.focus();
+    textArea.select();
+
+    try {
+      document.execCommand("copy");
+    } catch (err) {}
+
+    document.body.removeChild(textArea);
+  }
 }
